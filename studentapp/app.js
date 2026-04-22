c81529566144aa81823c68399b58944206ea2c8e
Akhilesh <pawarakhilesh22.com>
2026-04-22 23:15:38 +0530

    add file

---
+++ studentapp/app.js
@@ -0,0 +1,25 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/userdb')
+.then(()=> console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
